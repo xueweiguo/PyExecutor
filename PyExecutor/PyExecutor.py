dd4a28370d7c00c7689cee03da90221710cf4888
xueyuzhe <xue_weimin@yahoo.co.jp>
2018-11-25 20:49:23 +0900

    commit test

--- PyExecutor/PyExecutor.py
+++ PyExecutor/PyExecutor.py
@@ -8,7 +8,7 @@ from ElectricCooker.EcComponentFactory import *
 from ElectricCooker.EcSerializeFactory import *
 from PyExecutorFactory import *
 from SelModeDlg import *
-
+#test
 if __name__ == '__main__':
     main_wnd = PyExecuteWnd('PyExecutor V0.1')
     main_wnd.mainloop()
