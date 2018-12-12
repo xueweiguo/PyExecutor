ea45e80aaf19c7a68ba333d70de499c86373a8e1
xwg <weiguo_xue@hotmail.com>
2018-12-12 23:09:38 +0800

    添加TreeView关联注释。

--- ExFramework/ExTreeAccessor.py
+++ ExFramework/ExTreeAccessor.py
@@ -2,30 +2,38 @@ class ExTreeAccessor:
     def __init__(self):
         self.__observer__ = None
 
+    #取得根节点
     def get_root(self):
         pass
 
+    #取得父节点
     def get_parent(self, node):
         pass
 
+    #取得下级节点
     def get_children(self, node):
         pass
-
+    #取得节点名称
     def get_name(self, node):
         pass
 
+    # 取得节点iid
     def get_iid(self, node):
         pass
 
+    # 取得节点图标
     def get_image(self, node):
         pass
 
+    #聚焦节点
     def focus(self, iid):
         pass
 
+    #设定Observer
     def attach_observer(self, observer):
         self.__observer__ = observer
 
+    #节点操作通知
     def notify(self, invoker, ext):
         if self.__observer__:
             self.__observer__.update(invoker, ext)
--- PyExecutor/PyExecutorTreeAccessor.py
+++ PyExecutor/PyExecutorTreeAccessor.py
@@ -21,27 +21,35 @@ class PyExecutorTreeAccessor(ExTreeAccessor, ExObserver):
     def get_parent(self, node):
         return node.parent()
 
-    def get_children(self, node):
-        children = []
-        if isinstance(node, ExDiagram):
-            for c in node.children():
-                if isinstance(c, ExBlock):
-                    children.append(c)
-        elif isinstance(node, ExCustomBlock):
-            children.append(node.diagram())
-        return children
-
+    #取得节点名称
     def get_name(self, node):
         return node.name()
-
-    def get_iid(self, node):
-        return node.tag()
-
+    # 取得节点图标
     def get_image(self, node):
+        #根据节点类型返回图标
         if isinstance(node, ExDiagram):
             return self.__diagram_image__
         else:
             return self.__blk_image__
+    #取得节点iid
+    def get_iid(self, node):
+        #返回ExComponent的tag
+        return node.tag()
+    # 取得下级节点
+    def get_children(self, node):
+        children = []
+        #如果时ExDiagram对象，返回下级ExBlock节点
+        if isinstance(node, ExDiagram):
+            for c in node.children():
+                if isinstance(c, ExBlock):
+                    children.append(c)
+        # 如果时ExCustomBlock对象，返回内部的ExDiagram
+        elif isinstance(node, ExCustomBlock):
+            children.append(node.diagram())
+        else:
+            #其他类型节点，表示对象外。
+            pass
+        return children
 
     def focus(self, iid):
         component = ExComponentDict().component(iid)
