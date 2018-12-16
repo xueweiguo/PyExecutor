3583ae954ed06ad5c4e3371a3fb5e20c3b094904
xwg <weiguo_xue@hotmail.com>
2018-12-16 22:51:30 +0800

    Add new file.

---
+++ ExFramework/ExComposite.py
@@ -0,0 +1,56 @@
+from ExFramework.ExComponent import *
+
+class ExComposite(ExComponent):
+    def __init__(self, parent, name, comment):
+        ExComponent.__init__(self, parent, name, comment)
+        self.__children = []
+
+    def construct(self):
+        return ExComponent.construct(self)
+
+    #添加子要素
+    def append(self, child):
+        self.__children.append(child)
+        self.handle_request(child, 'append')
+
+    #移除子要素
+    def remove(self, child):
+        self.handle_request(child, 'remove')
+        self.__children.remove(child)
+
+    #将对象挂接到Canvas
+    def attach_canvas(self, canvas):
+        ExComponent.attach_canvas(self, canvas)
+        for c in self.children():
+            c.attach_canvas(canvas)
+    #从Canvas分离要素
+    def detach_canvas(self):
+        for c in self.children():
+            c.detach_canvas()
+        ExComponent.detach_canvas(self)
+
+    #系列化要素
+    def serialize(self):
+        return ExComponent.serialize(self)
+        for c in self.children():
+            c.serialize()
+
+    #取得子节点列表
+    def children(self):
+        return self.__children
+
+    def countChild(self, child_class):
+        count = 0
+        for child in self.children():
+            if isinstance(child, child_class):
+                count = count + 1
+        return count
+
+    def findChild(self, name):
+        for child in self.children():
+            if child.name() == name:
+                return child
+        return None
+
+
+
