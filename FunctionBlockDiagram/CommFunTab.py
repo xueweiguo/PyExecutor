1d8dda6c0248a374adf3003321b1f6860a1a94a8
xwg <weiguo_xue@hotmail.com>
2018-12-30 11:06:06 +0800

    修改CommFunTab布局。

--- FunctionBlockDiagram/CommFunTab.py
+++ FunctionBlockDiagram/CommFunTab.py
@@ -10,24 +10,46 @@ class CommFunTab(ExPropertyTab):
     def __init__(self, parent, name, element):
         ExPropertyTab.__init__(self, parent, name, element)
         self.pack(side=TOP)
-        Label(self, text="通讯方式").grid(row=0, column=0, padx = 1, pady = 1, sticky=E)
+
+        pad = 2
+
+        # 创建一个容器,
+        port_frame = LabelFrame(self, text=" 端口设定 ")  # 创建一个容器，其父容器为win
+        port_frame.grid(column=0, row=0, sticky=W, padx = pad, pady = pad)
+        Label(port_frame, text="通讯方式").grid(row=0, column=0, padx = pad, pady = pad, sticky=E)
         self.port = StringVar()
-        port_list = Combobox(self, width = 12, textvariable=self.port)
+        port_list = Combobox(port_frame, width = 12, textvariable=self.port)
         port_list['values'] = ('未指定','Wifi','Bluetooth')
         port_list.state = 'readonly'
         port_list.current(0)
-        port_list.grid(column=1, row=0, sticky=W, padx = 1, pady = 1)
-        Label(self, text="加密方式").grid(row=1, column=0, sticky=E, padx = 1, pady = 1)
+        port_list.grid(column=1, row=0, sticky=W, padx = pad, pady = pad)
+        Label(port_frame, text='通讯地址').grid(row=2, column=0, sticky=E, padx=pad, pady=1)
+        self.address = StringVar()
+        address = Entry(port_frame, textvariable=self.address)
+        address.grid(row=2, column=1, sticky=W, padx=pad, pady=pad)
+
+        data_frame = LabelFrame(self, text=" 数据处理 ", width = 200)  # 创建一个容器，其父容器为win
+        data_frame.grid(column=0, row=1, sticky=W, padx = pad, pady = pad)
+
+        Label(data_frame, text="加密方式").grid(row=0, column=0, sticky=E, padx = pad, pady = pad)
         self.security = StringVar()
-        security_list = Combobox(self, width=12, textvariable=self.security)
-        security_list['values'] = ('未指定', 'Base64', 'MD5')
+        security_list = Combobox(data_frame, width=12, textvariable=self.security)
+        security_list['values'] = ('不加密', 'DES', 'IDEA', 'RSA', 'DSA', 'AES')
         security_list.state = 'readonly'
         security_list.current(0)
-        security_list.grid( row=1, column=1, sticky=W, padx = 1, pady = 1)
-        Label(self, text='通讯地址').grid(row=2, column=0, sticky=E, padx = 1, pady = 1)
-        self.address = StringVar()
-        address = Entry(self, textvariable=self.address)
-        address.grid(row=2, column=1, sticky=W, padx = 1, pady = 1)
+        security_list.grid( row=0, column=1, sticky=W, padx = pad, pady = pad)
+        Label(data_frame, text='密钥').grid(row=1, column=0, sticky=E, padx=pad, pady=pad)
+        self.key = StringVar()
+        key = Entry(data_frame, textvariable=self.address)
+        key.grid(row=1, column=1, sticky=W, padx=pad, pady=pad)
+        Label(data_frame, text='校验方式').grid(row=2, column=0, sticky=E, padx=pad, pady=pad)
+        self.ecc = StringVar()
+        ecc_list = Combobox(data_frame, width=12, textvariable=self.ecc)
+        ecc_list['values'] = ('无校验','BCC', 'CRC', 'MD5')
+        ecc_list.state = 'readonly'
+        ecc_list.current(0)
+        ecc_list.grid(row=2, column=1, sticky=W, padx=pad, pady=pad)
+
 
     def apply(self):
         #设定通讯方式
