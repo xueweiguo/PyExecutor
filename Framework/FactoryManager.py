bc3f420dbbcf4d1b1e7fb51f3af91885dedccbd6
xwg <weiguo_xue@hotmail.com>
2019-08-04 23:07:00 +0800

    停止log输出

--- Framework/FactoryManager.py
+++ Framework/FactoryManager.py
@@ -30,7 +30,7 @@ class FactoryManager(object):
     # name:工厂名称
     # 模式有使用者指定
     def factory(self, name):
-        print(self.mode, name)
+        # print(self.mode, name)
         if self.mode == 'fbd':
             self.mode = 'Function Block Diagram'
         return self.__factory_dict.get(self.__key(self.mode, name))
