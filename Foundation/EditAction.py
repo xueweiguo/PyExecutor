bf3243078bd6e0ab47d0c8630f2ef17726e74205
xwg <weiguo_xue@hotmail.com>
2019-08-05 11:10:23 +0800

    为了便于调试，增加表示层次成员

--- Foundation/EditAction.py
+++ Foundation/EditAction.py
@@ -4,6 +4,10 @@ import abc
 class EditAction(metaclass=abc.ABCMeta):
     def __init__(self, component):
         self.component = component
+        self.level = 0
+
+    def set_level(self, level):
+        self.level = level
 
     @abc.abstractmethod
     def do(self):
