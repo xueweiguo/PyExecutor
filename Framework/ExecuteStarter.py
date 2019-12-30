962625dd220af3e3a05481581e12c5931cfe191e
weiguo xue <weiguo_xue@hotmail.com>
2019-12-30 23:00:44 +0800

    PyCharm升级后暴露的import错误对应 调整CustomBlock的Import位置。

--- Framework/ExecuteStarter.py
+++ Framework/ExecuteStarter.py
@@ -1,5 +1,4 @@
 from Framework.DiagramVisitor import *
-from Framework.CustomBlock import *
 
 
 class ExecuteStarter(DiagramVisitor):
@@ -7,6 +6,7 @@ class ExecuteStarter(DiagramVisitor):
         self.context = context
 
     def visit_block(self, block):
+        from Framework.CustomBlock import CustomBlock
         if isinstance(block, CustomBlock):
             return True
         else:
