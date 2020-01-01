c113a1e3fdb0df800367f0a88c272218ae13a34f
weiguo xue <weiguo_xue@hotmail.com>
2020-01-01 21:22:08 +0800

    不能选择输入引脚问题对应

--- Framework/InputPortFinder.py
+++ Framework/InputPortFinder.py
@@ -1,5 +1,6 @@
 from Framework.DiagramView import *
 
+
 class InputPortFinder(DiagramVisitor):
     def __init__(self, x, y):
         self.__x = x
@@ -8,9 +9,10 @@ class InputPortFinder(DiagramVisitor):
 
     # 希望继续后序访问时返回True
     def visit_input(self, in_port):
-        pt = in_port.point()
-        if 0 < self.__x - pt[0] < 10 and abs(pt[1] - self.__y) < 2:
-            self.in_port = in_port
-            return False
+        if in_port.canvas:
+            pt = in_port.point()
+            if 0 < self.__x - pt[0] < 10 and abs(pt[1] - self.__y) < 2:
+                self.in_port = in_port
+                return False
         return True
 
