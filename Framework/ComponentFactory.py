9159c7bea7ed3787b9bef590e90630c1de54d6cf
weiguo xue <weiguo_xue@hotmail.com>
2020-01-01 21:19:06 +0800

    初始化参数调整

--- Framework/ComponentFactory.py
+++ Framework/ComponentFactory.py
@@ -11,18 +11,18 @@ class ComponentFactory(object):
 
     # 取得支持的要素类型
     def element_types(self):
-        return ['Custom', 'Input', 'Output','Note']
+        return ['Custom', 'Input', 'Output', 'Note']
 
     # 建要素
-    def make_element(self, cd, type):
+    def make_element(self, cd, parent, type):
         if type == "Custom":
-            return CustomBlock(cd, 'Custom').construct(None)
+            return CustomBlock(cd, 'Custom').construct(parent)
         elif type == "Input":
-            return InputBlock(cd, 'Input', '用户取得自定义功能的输入数据').construct(None)
+            return InputBlock(cd, 'Input', '用户取得自定义功能的输入数据').construct(parent)
         elif type == "Output":
-            return OutputBlock(cd, 'Output','用于设定用户自定义模块的输出数据').construct(None)
+            return OutputBlock(cd, 'Output','用于设定用户自定义模块的输出数据').construct(parent)
         elif type == "Note":
-            return Note(cd).construct(None)
+            return Note(cd).construct(parent)
 
     # 生成运行上下文
     def make_context(self):
